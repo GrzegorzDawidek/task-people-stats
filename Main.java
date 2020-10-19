04066932ee1eda6163fab7804ac0671c2b5f198c
Your Name <grzesiek10014@gmail.com>
2020-10-19 20:01:40 +0200

    initialize repo

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 package com.company;
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+	System.out.println("Hello world");
+		 
+    PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
+
+    System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
+}
 }
 
 class PeopleStats {
@@ -18,6 +22,9 @@ class PeopleStats {
             throw new IllegalStateException(e);
         }
     }
+	public long count() {
+    return people.size();
+}
 }
 
 class Person {
